0aee40a14c7f6150c886b6eaa7c0913d9196963b
Carter Rennick <carter.rennick@gmail.com>
2021-03-27 00:53:32 -0400

    Fixed project files AGAIN

--- build/revision.h
+++ build/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xfc90f5c7
+#define GitVersion 0xa2f56aed
 #define GitBranch "master"
 
 #define VER_MAJOR 5
@@ -16,13 +16,13 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit fc90f5c7)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit a2f56aed)"
 #endif
 
 #ifdef  NDEBUG
-#define VER_ORIGINAL_FILENAME_STR   "assimp-vc142-mt.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
 #else
-#define VER_ORIGINAL_FILENAME_STR   "assimp-vc142-mtd.dll"
+#define VER_ORIGINAL_FILENAME_STR   "assimpd.dll"
 #endif //  NDEBUG
 
 #endif // ASSIMP_REVISION_H_INC
